82095f8f915d71282e8bdb4a8e57728c10203068
Zhlobo Dmitry <dmitry.zhlobo@yandex.ru>
2022-09-30 12:01:29 +0300

    Update Readme.md, token, response.ok

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
 import requests
-from pprint import pprint
-from urllib.request import urlopen, URLError
 from urllib.parse import urlsplit
 import os
 from dotenv import load_dotenv
 
 
 def shorten_link(url, token):
-    headers = {'Authorization': token}
+    headers = {'Authorization': f'Bearer {token}'}
     create_bitlink = 'https://api-ssl.bitly.com/v4/bitlinks'
     body = {"long_url": url}
     response = requests.post(create_bitlink, headers=headers, json=body)
-    response.raise_for_status()
-    return response.json()['link']
+    if response.ok:
+        return response.json()['link']
 
 
 def count_clicks(bitlink, token):
     headers = {'Authorization': token}
     parced_url = urlsplit(bitlink)
-    bitlink_for_clicks = f'{parced_url[1]}{parced_url[2]}'
+    bitlink_for_clicks = f'{parced_url.netloc}{parced_url.path}'
     url_for_clicks = f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink_for_clicks}/clicks/summary'
     response = requests.get(url_for_clicks, headers=headers)
-
-    return response.json()['total_clicks']
+    if response.ok:
+        return response.json()['total_clicks']
 
 
 def is_bitlink(url, token):
@@ -32,13 +30,21 @@ def is_bitlink(url, token):
     bitlink_for_check = f'{parced_url.netloc}{parced_url.path}'
 
     response = requests.get(f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink_for_check}', headers=headers)
-    if response.ok:
-        return response.json()
+    return response.ok
 
 
 def main():
     load_dotenv()
-    TOKEN = os.getenv('BITLY_TOKEN')
+    token = os.getenv('BITLY_TOKEN')
+
+    link = input('Введите полную ссылку: ')
+    bitlink = shorten_link(link, token)
+    print(bitlink)
+
+    print(is_bitlink(bitlink, token))
+
+    clicks = count_clicks(bitlink, token)
+    print(clicks)
 
 
 if __name__ == '__main__':
